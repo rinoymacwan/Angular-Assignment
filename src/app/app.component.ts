366c45d78218954abd91b484500cf3bd9dd3c2f6
rinoy <rinoy@promactinfo.com>
2019-09-02 15:07:59 +0530

    two way with model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'learning';
+  str: string;
+  emp: Employee;
+  ngOnInit(): void {
+    this.str = 'wertttt';
+    this.emp = new Employee();
+    this.emp.firstName = 'yeahh';
+  }
+  func(): void {
+    console.log(this.str);
+  }
+  func1(): void {
+    console.log(this.emp.firstName);
+  }
 }
---
+++ src/app/employee.ts
@@ -0,0 +1,13 @@
+export class Employee {
+  firstName: string;
+  lastName: string;
+  email: string;
+  contactNumber: number;
+  address: string;
+  username: string;
+  password: string;
+  gender: string;
+  qualification: string;
+  experience: string;
+  codingLangauges: string[];
+}
